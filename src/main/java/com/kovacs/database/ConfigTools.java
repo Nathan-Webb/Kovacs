3c27d15d87111066f6892309874edd8a45817ea5
Nathan <nathanwebb7550@gmail.com>
2020-04-05 10:36:51 -0500

    minor edits

--- src/main/java/com/kovacs/database/ConfigTools.java
+++ src/main/java/com/kovacs/database/ConfigTools.java
@@ -53,33 +53,25 @@ public class ConfigTools {
     }
 
     public static boolean canUseBot(Member member){
-        if(isSudo(member)){
-            return true;
-        }
         return !cantUseBot(member);
     }
     public static boolean cantUseBot(Member member){
         String authorID = member.getId();
         String guildID = member.getGuild().getId();
         GuildConfig config = GuildConfig.get(guildID);
-        Member guildOwner = member.getGuild().getOwner();
-        String guildOwnerID;
-        if(guildOwner ==  null){
-            guildOwnerID = "";
-        } else {
-            guildOwnerID = guildOwner.getId();
+        if(isSudo(member)){
+            return false;
         }
 
-        if(!config.getSudoUsers().contains(authorID) && !guildOwnerID.equals(authorID)){ //user isn't a bot owner
-            if(!config.getWhitelistedUsers().contains(authorID)){ //user isn't whitelisted
-                if(member.getRoles().stream()
-                        .noneMatch(role -> config.getSudoRoles().contains(role.getId()))){ //none of the users roles are sudo
-                    return member.getRoles().stream()
-                            .noneMatch(role -> config.getWhitelistedRoles().contains(role.getId())); //are any of the roles whitelisted?
-                }
-
+        if(!config.getWhitelistedUsers().contains(authorID)){ //user isn't whitelisted
+            if(member.getRoles().stream()
+                    .noneMatch(role -> config.getSudoRoles().contains(role.getId()))){ //none of the users roles are sudo
+                return member.getRoles().stream()
+                        .noneMatch(role -> config.getWhitelistedRoles().contains(role.getId())); //are any of the roles whitelisted?
             }
+
         }
+
         return false;
     }
 }
